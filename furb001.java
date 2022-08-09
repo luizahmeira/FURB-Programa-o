8684ff1617cc5ce0a0f71729d6d107751ba69c77
Luiza Helena Meira <108182168+luizahmeira@users.noreply.github.com>
2022-08-09 20:26:17 -0300

    Add files via upload

---
+++ furb001.java
@@ -0,0 +1,26 @@
+/*enunciado
+ * 
+ * um professor deseja calcular a média de um aluno. E o professor tem o nome e as 3 notas
+ *  do aluno, e a media deve ser calculada de forma aritmética
+ */
+
+ //ter as notas do aluno
+//double aceita casas decimais
+public class furb001 {
+    public static void main(String[] args) {
+        //entradas
+        double nota1 = 7.5;
+        double nota2 = 8;
+        double nota3 = 9;
+    
+        //Processo
+        double media = (nota1 + nota2 + nota3)/3;
+    
+        //Saída
+        System.out.print("Sua média é: "+ media);
+    }
+}
+
+//public class tem q ter o mesmo nome do arquivo
+
+
