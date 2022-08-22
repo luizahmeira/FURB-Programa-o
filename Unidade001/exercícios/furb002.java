a5a367312ec2293f87785c5fc5b4bd0cae5143d2
Luiza Helena Meira <108182168+luizahmeira@users.noreply.github.com>
2022-08-22 20:18:55 -0300

    Add files via upload

---
+++ Unidade001/exercícios/furb002.java
@@ -0,0 +1,21 @@
+/*Modele a solução de um problema para calcular a área de uma sala retangular.  
+Considere que as medidas estão expressas em metros */
+
+public class furb002 {
+
+    public static void main(String[] args) {
+        //Entrada
+        double largura = 5;
+        double altura = 3;
+
+        //Processo
+        double area = (largura * altura);
+
+        //Saída
+        System.out.print("A área é"+ area);
+    }
+
+}
+
+
+
---
+++ Unidade001/exercícios/furb003.java
@@ -0,0 +1,9 @@
+public class furb003 {
+    
+  public static void main(String[] args) {
+    System.out.println("Olá"); 
+    // 
+        System.out.print("Olá mundo");
+        // 
+  }
+}
